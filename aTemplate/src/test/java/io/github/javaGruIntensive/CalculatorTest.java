3cf708b4f3790891374ffce48863a87cb1462bd6
goldenas18 <golden18520@gmail.com>
2021-02-13 10:31:35 +0200

    initial commit.

---
+++ aTemplate/src/test/java/io/github/javaGruIntensive/CalculatorTest.java
@@ -0,0 +1,31 @@
+package io.github.javaGruIntensive;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        assertEquals(20,20 );
+
+    }
+
+    @Test
+    void sub() {
+        assertEquals(20,20 );
+    }
+
+    @Test
+    void div() {
+        assertEquals(20,20 );
+    }
+
+    @Test
+    void mul() {
+        assertEquals(20,20 );
+    }
+}
