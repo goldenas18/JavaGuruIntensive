aa1004e8ba90eb7f53cd2dff58b41164769d28b0
goldenas18 <golden18520@gmail.com>
2021-02-13 13:27:22 +0200

    4d commit.

--- aTemplate/src/test/java/io/github/javaGruIntensive/CalculatorTest.java
+++ aTemplate/src/test/java/io/github/javaGruIntensive/CalculatorTest.java
@@ -2,26 +2,27 @@ package io.github.javaGruIntensive;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 class CalculatorTest {
-
+    int x = 20;
+    int y = 10;
     @Test
     void add() {
-        assertEquals(20,20 );
-    }
-    @Test
-    void sub() {
-        assertEquals(20,20 );
-    }
-    @Test
-    void div() {
-        assertEquals(20,20 );
-    }
-    @Test
-    void mul() {
-        assertEquals(20,20 );
+        assertEquals(30, Calculator.add(x, y));
     }
 
+    @Test
+    void sub() {
+        assertEquals(10, Calculator.sub(x, y));
+    }
+
+    @Test
+    void div() {
+        assertEquals(2, Calculator.div(x, y));
+    }
+
+    @Test
+    void mul() {
+        assertEquals(200, Calculator.mul(x, y));
+    }
 }
